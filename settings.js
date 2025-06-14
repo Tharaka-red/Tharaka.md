e334e9ac3baf98aa0378b8e868ab9f76bb2f1da9
Tharaka-red <t2008l1222@gmail.com>
2025-06-14 21:26:13 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Red dragan bot',
+  author: '‎',
+  botName: "Red dragan bot",
+  botOwner: 'Tharaka', // Your name
+  ownerNumber: '94786908427', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.3",
+};
+
+module.exports = settings;
